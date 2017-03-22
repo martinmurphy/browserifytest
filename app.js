ce2c0d34cd83c916ff87fc01c861daec4dc83e8a
Martin Murphy <mm@martinmurphy.com>
2017-03-22 09:11:44 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var jQuery = require('jquery');
+window.$ = window.jQuery = jQuery;
+require('bootstrap');
+var dt = require('datatables.net')();
+
+require('datatables.net-bs');
+
+require('./app.css');
+
+$('#doit').click(function (){
+  $('#response').html('Button was pressed');
+     var t = $('#example').DataTable();
+            t.row.add( [
+            'joebloggs',
+            'joebloggs'
+        ] ).draw( false );
+});
